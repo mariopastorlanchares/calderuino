9fc9545ec75933775705fa44d88e19b5381bf1a5
Mario Pastor <mariopastorlanchares@gmail.com>
2022-04-14 17:32:36 +0200

    feat - Firebase implemented

--- README.md
+++ README.md
@@ -10,9 +10,20 @@ Calderuino is a project to monitor and manage multiple boilers via a web applica
 Create a **secrets.h** header file for arduino project in **/calderuino/** folder (the folder where calderuino.ino is
 placed) to store all your passwords for wifi and firebase connections.
 
-Copy this contents:
+Copy this contents in secrets.h:
 
  ```
+/* 1. Define the Wifi credentials */
 #define SECRET_SSID "YOUR_SSID"
-#define SECRET_PASS "your_password"
+#define SECRET_PASS "YOUR_PASSWORD"
+
+/* 2. Define the API Key */
+#define API_KEY"AIza*********"
+
+/* 3. Define the RTDB URL */
+#define DATABASE_URL "******.firebasedatabase.app" //<databaseName>.firebaseio.com or <databaseName>.<region>.firebasedatabase.app
+
+/* 4. Define the user Email and password that alreadey registerd or added in your project */
+#define USER_EMAIL "*********@****.com"
+#define USER_PASSWORD "***********"
  ```
--- calderuino/calderuino.ino
+++ calderuino/calderuino.ino
@@ -1,11 +1,28 @@
+// #include <WiFi.h>
 #include <NTPClient.h>
-#include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
+// #include <ESP8266WiFi.h>
 #include "secrets.h"
 
-// Create a "secrets.h" header file in the project root and store the passwords there
-const char *ssid     = SECRET_SSID;
-const char *password = SECRET_PASS;
+#if defined(ESP32)
+#include <WiFi.h>
+#elif defined(ESP8266)
+#include <ESP8266WiFi.h>
+#endif
+
+#include <Firebase_ESP_Client.h>
+
+// Provide the token generation process info.
+#include <addons/TokenHelper.h>
+
+//Define the Firebase Data object
+FirebaseData fbdo;
+
+//Define the FirebaseAuth data for authentication data
+FirebaseAuth auth;
+
+// Define the FirebaseConfig data for config data
+FirebaseConfig config;
 
 char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 
@@ -13,28 +30,61 @@ char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursd
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "pool.ntp.org");
 
-void setup(){
-  Serial.begin(115200);
-
-  WiFi.begin(ssid, password);
+void setup() {
+  Serial.begin(9600);
+  Serial.print("Hola ");
+  // WiFi
+  WiFi.begin( SECRET_SSID, SECRET_PASS);
 
   while ( WiFi.status() != WL_CONNECTED ) {
     delay ( 500 );
     Serial.print ( "." );
   }
+  // Time client
+  // timeClient.begin();
 
-  timeClient.begin();
+  // Firebase
+  Serial.printf("Firebase Client v%s\n\n", FIREBASE_CLIENT_VERSION);
+
+  /* Assign the api key (required) */
+  config.api_key = API_KEY;
+
+  /* Assign the user sign in credentials */
+  auth.user.email = USER_EMAIL;
+  auth.user.password = USER_PASSWORD;
+
+  /* Assign the RTDB URL (required) */
+  config.database_url = DATABASE_URL;
+
+  /* Assign the callback function for the long running token generation task */
+  config.token_status_callback = tokenStatusCallback; // see addons/TokenHelper.h
+
+  //Initialize the library with the Firebase authen and config.
+  Firebase.begin(&config, &auth);
+
+  //Optional, set AP reconnection in setup()
+  Firebase.reconnectWiFi(true);
+
+  Firebase.setDoubleDigits(5);
+
+  //Optional, set number of error retry
+  // Firebase.RTDB.setMaxRetry(&fbdo, 3);
+
+  //Optional, set number of error resumable queues
+  // Firebase.RTDB.setMaxErrorQueue(&fbdo, 30);
+
+  //Optional, use classic HTTP GET and POST requests.
+  //This option allows get and delete functions (PUT and DELETE HTTP requests) works for
+  //device connected behind the Firewall that allows only GET and POST requests.
+  // Firebase.RTDB.enableClassicRequest(&fbdo, true);
 }
 
 void loop() {
   timeClient.update();
-
-  Serial.print(daysOfTheWeek[timeClient.getDay()]);
   Serial.print(timeClient.getEpochTime());
   Serial.print(" ");
-  Serial.print(timeClient.getMinutes());
-  Serial.println("");
-  //Serial.println(timeClient.getFormattedTime());
-
-  delay(1000);
+  Serial.println(timeClient.getMinutes());
+  Serial.printf("Set int... %s\n", Firebase.RTDB.setInt(&fbdo, F("time"), timeClient.getEpochTime()) ? "ok" : fbdo.errorReason().c_str());
+  // Serial.printf("Set int... %s\n", Firebase.RTDB.setInt(&fbdo, F("/test/int"), 1) ? "ok" : fbdo.errorReason().c_str());
+  delay(10000);
 }
